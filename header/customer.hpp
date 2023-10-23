2162699152c557f53a970eb6a99802edbb1132de
janripken <janripken@protonmail.com>
2023-10-23 18:03:52 +0200

    cleared build

---
+++ header/customer.hpp
@@ -0,0 +1,15 @@
+#ifndef CUSTOMER_HPP
+#define CUSTOMER_HPP
+#include <string>
+class Customer {
+public:
+private:
+  std::string Name;
+  std::string Straße;
+  std::string Hausnummer;
+  std::string Postleitzahl;
+  std::string Wohnort;
+  float Rechnungsnummer;
+};
+
+#endif
